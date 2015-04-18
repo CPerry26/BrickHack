2841af672df48c9926cc32a491e34818ac6c43ac
John <johnlavoi@gmail.com>
2015-04-18 15:35:47 -0400

    adding base class implementations

---
+++ src/Leaderboard.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+
+public class Leaderboard {
+	private ArrayList<User> board;
+	
+	public Leaderboard(){
+		
+	}
+}
--- src/Question.java
+++ src/Question.java
@@ -1,4 +1,9 @@
 
 public class Question {
-
+	private int time;
+	private int id;
+	private String questionText;
+	private String answer;
+	private int pointValue;
+	private int difficulty;
 }
