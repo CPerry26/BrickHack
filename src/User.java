8ad42d4c21ccf4afd0c3798a6f04818d14becf90
Cody <codyy@live.com>
2015-04-18 17:24:15 -0400

    Added getter functions to the User class

--- src/User.java
+++ src/User.java
@@ -1,9 +1,34 @@
 import java.util.*;
 
 public class User {
-	private int id;
-	private String name;
-	private String highScore;
-	private int rank;
+	public int id;
+	public String name;
+	public String highScore;
+	public int rank;
 	private Hashtable<String, Integer> topicRanks = new Hashtable<String, Integer>();
+	
+	public User(int id1, String name1, String highScore1, int rank1){
+		id = id1;
+		name = name1;
+		highScore = highScore1;
+		rank = rank1;
+		
+	}
+	
+	public int getID(){
+		return id;
+	}
+	
+	public String getName(){
+		return name.toString();
+	}
+	
+	public String getHighscore(){
+		return highScore.toString();
+	}
+	
+	public int getRank(){
+		return rank;
+	}
+	
 }
