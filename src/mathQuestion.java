825c98ded90b340b7e3959116462dbf36aabb586
William <wtc9653@g.rit.edu>
2015-04-18 19:41:40 -0400

    Implemented askQuesiton() and getUserAnswer() in mathQuestion.  Also added a getInput() function to Question.

--- src/mathQuestion.java
+++ src/mathQuestion.java
@@ -1,3 +1,5 @@
+import java.util.Timer;
+
 /*
  * The math question class
  * 
@@ -6,16 +8,36 @@
 public class mathQuestion extends Question {
 	int a;
 	int b;
-	int timelimit;
-	String answer;
-	String questionText;
+	long startTime;
+	long elapsedTime;
 	
-	public void makeMathQuestion( int size, int timelimit ) {
+	public mathQuestion( int size, int timelimit ) {
 		//This might need to return a question.  I'm not sure...
 		int a = (int) ( size * Math.random() );
 		int b = (int) ( size * Math.random() );
-		String answer = Integer.toString( a + b );
-		String quesitonText = ("What is "+ Integer.toString(a) +" + "+ Integer.toString(b) +"?");
-		return;
+		setAnswer( "" + Integer.toString( a + b ) );
+		setQuestionText( "What is "+ Integer.toString(a) +" + "+ Integer.toString(b) +"?" );
 	}
+	
+	public void askQuestion() {
+		System.out.println( "You will have " + getTime() + "seconds to answer this question." );
+		//start the timer
+		startTime = System.currentTimeMillis();
+		elapsedTime = 0L;
+		//ask the user the question
+		System.out.println( getQuestionText() );
+		
+	}
+	
+	public boolean getUserAnswer() {
+		String UserAnswer = getInput();
+		elapsedTime = startTime - System.currentTimeMillis();
+		if( elapsedTime < (long)getTime()*1000 ) {
+			if( UserAnswer.equals( getAnswer() ) ) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 }
