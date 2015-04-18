37c561a25756ef69c0c715edc81922e89f926ef5
Cody <codyy@live.com>
2015-04-18 16:10:00 -0400

    Initial user object commit

--- src/User.java
+++ src/User.java
@@ -1,4 +1,9 @@
+import java.util.*;
 
 public class User {
-
+	private int id;
+	private String name;
+	private String highScore;
+	private int rank;
+	private Hashtable<String, Integer> topicRanks = new Hashtable<String, Integer>();
 }
