e070dd7fd50d6b00f54012ffe2da6654a0bb4319
William <wtc9653@g.rit.edu>
2015-04-18 17:48:19 -0400

    Added the rest of the setter/getter methods and added a getInput() function

--- src/Question.java
+++ src/Question.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Question {
 	private int time;
@@ -7,12 +8,51 @@ public class Question {
 	private int pointValue;
 	private int difficulty;
 	
+	public String getInput() {
+		Scanner sc = new Scanner( System.in );
+		String returnMe = sc.next();
+		sc.close();
+		return returnMe;
+	}
+	
+	public void setID( int id ) {
+		this.id = id;
+	}
+	
+	public void setPointValue( int pointValue ) {
+		this.pointValue = pointValue;
+	}
+	
+	public void setDifficulty( int difficulty ) {
+		this.difficulty = difficulty;
+	}
+	
+	public void setTime( int time ) {
+		this.time = time;
+	}
+	
 	public void setAnswer( String answer ) {
 		this.answer = answer;
 	}
 	
-	public void setQuestionText( String questionText ) {
-		this.questionText = questionText;
+	public void setQuestionText( String qText ) {
+		this.questionText = qText;
+	}
+	
+	public int getID() {
+		return id;
+	}
+	
+	public int getPointValue() {
+		return pointValue;
+	}
+	
+	public int getDifficulty() {
+		return difficulty;
+	}
+	
+	public int getTime() {
+		return time;
 	}
 	
 	public String getAnswer() {
